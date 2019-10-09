46553ad5a0656fceae1c85fcef0e18e54e259fa4
D47K <skorpio1197@gmail.com>
2019-10-09 14:38:53 -0500

    Primer diseño

--- js/index.js
+++ js/index.js
@@ -5,64 +5,80 @@ var Nav = function Nav() {
     { className: "row header-profile bg-dark" },
     React.createElement(
       "div",
-      { className: "col-12 d-flex justify-content-center" },
+      { className: "col-12 d-flex justify-content-center person-image p-0" },
       React.createElement("img", {
-        className: "rounded-circle img-profile m-1",
+        className: "rounded-circle img-profile",
         src: "https://scontent.fmex10-2.fna.fbcdn.net/v/t1.0-9/30707719_1640298776039966_7798285058170893456_n.jpg?_nc_cat=110&_nc_oc=AQlbDbve5jIeefb0WBre4D-cBZrzZvMsgpITpPP4UHDFMOcZsGpF8uuaIOniGA0veYwHiYl8n1sJy7yyMyXpS4dF&_nc_ht=scontent.fmex10-2.fna&oh=2fbfefa5fa3f9f057a9a52b45a1c36ec&oe=5E2B9C93"
       })
     ),
     React.createElement(
       "div",
-      { className: "col-12 " },
+      { className: "col-12  d-flex  justify-content-center    " },
       React.createElement(
         "div",
-        { className: "row" },
+        { className: "container person-card shadow-box  d-flex align-items-end p-1 rounded" },
         React.createElement(
           "div",
-          { className: "col-12 " },
+          null,
           React.createElement(
-            "h4",
-            { className: "text-white text-center" },
-            "Andres Hilario Vidal"
-          )
-        )
-      ),
-      React.createElement(
-        "div",
-        { className: "row" },
-        React.createElement(
-          "div",
-          { className: "col-12 " },
-          React.createElement(
-            "h6",
-            { className: "text-white text-center" },
-            "Desarrollador WEB, aplicaciones m\xF3viles y aplicaciones de escritorio"
-          )
-        )
-      ),
-      React.createElement(
-        "div",
-        { className: "row" },
-        React.createElement(
-          "div",
-          { className: "col-12 d-flex justify-content-end" },
-          React.createElement(
-            "a",
-            { href: "https://github.com/D47K" },
-            React.createElement("img", {
-              src: "../media/logos/icons8-github.svg",
-              className: "link",
-              alt: "github-logo"
-            })
+            "div",
+            { className: "row" },
+            React.createElement(
+              "div",
+              { className: "col-12 " },
+              React.createElement(
+                "h4",
+                { className: "text-center" },
+                "Andres Hilario Vidal"
+              )
+            )
           ),
           React.createElement(
-            "a",
-            { href: "https://www.facebook.com/andres.hilariovidal" },
-            React.createElement("img", {
-              src: "../media/logos/icons8-facebook.svg",
-              className: "link-fb",
-              alt: "facebook-logo"
-            })
+            "div",
+            { className: "row" },
+            React.createElement(
+              "div",
+              { className: "col-12 " },
+              React.createElement(
+                "h6",
+                { className: "text-center" },
+                "Desarrollador WEB, aplicaciones m\xF3viles y aplicaciones de escritorio"
+              )
+            )
+          ),
+          React.createElement(
+            "div",
+            { className: "row" },
+            React.createElement(
+              "div",
+              { className: "col-12 logos-social " },
+              React.createElement(
+                "div",
+                { className: "row" },
+                React.createElement(
+                  "div",
+                  { className: "col-12 d-flex justify-content-center" },
+                  React.createElement(
+                    "a",
+                    { href: "https://github.com/D47K" },
+                    React.createElement("img", {
+                      src: "../media/logos/icons8-github.svg",
+                      className: "link",
+                      alt: "github-logo"
+                    })
+                  ),
+                  React.createElement(
+                    "a",
+                    { href: "https://www.facebook.com/andres.hilariovidal" },
+                    React.createElement("img", {
+                      src: "../media/logos/icons8-facebook.svg",
+                      className: "link-fb",
+                      alt: "facebook-logo"
+                    })
+                  )
+                )
+              )
+            )
           )
         )
       )
